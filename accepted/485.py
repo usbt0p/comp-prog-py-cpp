5edeb2d8a06526e0c559228e6aae0c7ce2cbf434
usbt0p <lucascdeaa@gmail.com>
2025-06-03 19:25:02 +0200

    golf that shii

--- accepted/485.py
+++ accepted/485.py
@@ -1,3 +1,11 @@
+#https://aceptaelreto.com/problem/statement.php?id=485
+
+# golf that shit
+while int(input()): print(*[sum(E[e:]) for e in range(len(E))]) if (E := [*map(int, input().split())]) else _
+
+
+# Soluciçón legible:
+'''
 n_etapas = int(input())
 
 while n_etapas != 0:
@@ -7,3 +15,4 @@ while n_etapas != 0:
     print(*[sum(etps[e:]) for e in range(len(etps))])
 
     n_etapas = int(input())
+'''
